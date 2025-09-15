e267435e519770297c12708b258c54960e801fe3
Sinsina V <sinsinav505@gmail.com>
2025-09-15 20:27:12 +0530

    Add files via upload

---
+++ sep15d.py
@@ -0,0 +1,17 @@
+class calc:
+    def add(self,a,b):
+        c=a+b
+        return c
+    def sub(self,a,b):
+        c=a*b
+        return c
+    def div(self,a,b):
+        c=a/b
+        return c
+    def mul(self,a,b):
+        c=a*b
+        return c
+    
+class show:
+    def display(self):
+        print("Hi")
